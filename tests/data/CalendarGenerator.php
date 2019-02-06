cbc0ada763c0de5bb1eabe5a72e4974a16672292
Seth Battis <seth@battis.net>
2019-02-05 22:15:40 -0500

    Improved test data generation

---
+++ tests/data/CalendarGenerator.php
@@ -0,0 +1,180 @@
+<?php
+
+
+namespace Battis\IcsMunger\Tests\data;
+
+
+use DateInterval;
+use DateTime;
+use Exception;
+use kigkonsult\iCalcreator\vcalendar;
+use kigkonsult\iCalcreator\vevent;
+use kigkonsult\iCalcreator\vtimezone;
+
+class CalendarGenerator
+{
+    const START_RELATIVE = '-5 years';
+    const END_RELATIVE = '+5 years';
+
+    private static $US_EASTERN_TIMEZONE = <<<EOT
+BEGIN:VTIMEZONE
+TZID:America/New_York
+X-LIC-LOCATION:America/New_York
+BEGIN:STANDARD
+DTSTART:20191103T020000
+TZOFFSETFROM:-0400
+TZOFFSETTO:-0500
+RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=1SU;BYMONTH=11
+TZNAME:Eastern Standard Time
+END:STANDARD
+BEGIN:DAYLIGHT
+DTSTART:20190310T020000
+TZOFFSETFROM:-0500
+TZOFFSETTO:-0400
+RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=2SU;BYMONTH=3
+TZNAME:Eastern Daylight Time
+END:DAYLIGHT
+END:VTIMEZONE
+EOT;
+
+    /**
+     * @var vcalendar
+     */
+    private $base;
+
+    /**
+     * @var DateTime
+     */
+    private $start;
+
+    /**
+     * @var DateTime
+     */
+    private $end;
+
+    /**
+     * CalendarGenerator constructor.
+     * @param DateTime|null $start
+     * @param DateTime|null $end
+     * @param int $eventCount
+     * @throws Exception
+     */
+    public function __construct(DateTime $start = null, DateTime $end = null, int $eventCount = 1000)
+    {
+        $this->base = self::getEmptyCalendar();
+        if ($start === null) $start = new DateTime('-5 years');
+        if ($end === null) $end = new DateTime('+5 years');
+        if ($eventCount < 0) $eventCount = 0;
+        $this->generateRandomEvents($start, $end, $eventCount);
+    }
+
+    private static function getEmptyCalendar(): vcalendar
+    {
+        $c = new vcalendar(['unique_id' => __CLASS__]);
+        $c->setCalscale('GREGORIAN');
+        $c->setMethod('PUBLISH');
+        $tz = new vtimezone();
+        $tz->parse(self::$US_EASTERN_TIMEZONE);
+        $c->addComponent($tz);
+        return $c;
+    }
+
+    private function generateRandomEvents(DateTime $start, DateTime $end, int $eventCount): void
+    {
+        $this->start = $start;
+        $this->end = $end;
+        $start = $start->getTimestamp();
+        $end = $end->getTimestamp();
+        for ($i = 0; $i < $eventCount; $i++) {
+            $e = new vevent();
+            $e->setSummary(random_factor());
+            $e->setDescription(random_factor());
+            $e->setDtstart(date('c', rand($start, $end)));
+            $e->setDuration(0, 0, rand(0, 3), rand(1, 45));
+            $this->base->addComponent($e);
+        }
+    }
+
+    public function save(string $directory, string $filename): void
+    {
+        $this->base->setConfig('directory', $directory);
+        $this->base->setConfig('filename', $filename);
+        $this->base->saveCalendar();
+    }
+
+    /**
+     * @param callable $filter callable(vevent): bool
+     * @return CalendarGenerator
+     * @throws Exception
+     */
+    public function filter(callable $filter): CalendarGenerator
+    {
+        $result = new CalendarGenerator(null, null, 0);
+        $this->base->getComponent(0);
+        while ($e = $this->base->getComponent('vevent')) {
+            if ($filter($e)) {
+                $result->base->addComponent($e);
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * @param callable $transform callable(vevent): vevent
+     * @return CalendarGenerator
+     * @throws Exception
+     */
+    public function transform(callable $transform): CalendarGenerator
+    {
+        $result = new CalendarGenerator(null, null, 0);
+        $this->base->getComponent(0);
+        while ($e = $this->base->getComponent('vevent')) {
+            $result->base->addComponent($transform($e));
+        }
+        return $result;
+    }
+
+    /**
+     * @param int $count
+     * @param float $overlapPercentage
+     * @return CalendarGenerator[]
+     * @throws Exception
+     */
+    public function snapshots(int $count, float $overlapPercentage = 0.9): array
+    {
+        $result = [];
+        $window = (int)(($this->end->getTimestamp() - $this->start->getTimestamp()) / ($overlapPercentage + (($count - 1) * (1 - $overlapPercentage))));
+        $step = (int)((1 - $overlapPercentage) * $window);
+        $start = $this->start;
+        for ($i = 0; $i < $count; $i++) {
+            $snapshot = new CalendarGenerator(null, null, 0);
+            $end = clone $start;
+            $end->add($this->interval($window));
+            $this->base->getComponent(0);
+            foreach ($this->base->selectComponents($start, $end, null, null, null, null, 'vevent', true, true, false) as $e) {
+                $snapshot->base->addComponent($e);
+            }
+            array_push($result, $snapshot);
+            $start->add($this->interval($step));
+        }
+        return $result;
+    }
+
+    /**
+     * @param int $seconds
+     * @return DateInterval
+     * @throws Exception
+     */
+    private function interval(int $seconds): DateInterval
+    {
+        $years = (int)($seconds / (60 * 60 * 24 * 365));
+        if ($years) $seconds = $seconds % ($years * 60 * 60 * 24 * 365);
+        $days = (int)($seconds / (60 * 60 * 24));
+        if ($days) $seconds = $seconds % ($days * 60 * 60 * 24);
+        $hours = (int)($seconds / (60 * 60));
+        if ($hours) $seconds = $seconds % ($hours * 60 * 60);
+        $minutes = (int)($seconds / 60);
+        if ($minutes) $seconds = $seconds % ($minutes * 60);
+        return new DateInterval("P{$years}Y{$days}DT{$hours}H{$minutes}M{$seconds}S");
+    }
+}
--- tests/data/regenerate.php
+++ tests/data/regenerate.php
@@ -1,179 +1,38 @@
 <?php
 
-use kigkonsult\iCalcreator\vcalendar;
+use Battis\IcsMunger\Tests\data\CalendarGenerator;
 use kigkonsult\iCalcreator\vevent;
-use kigkonsult\iCalcreator\vtimezone;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-define('START_YEAR', 2015);
-define('END_YEAR', 2025);
-define('SNAPSHOTS', 3);
-
-define('SNAPSHOT_MODE_SIMPLE', 0);
-define('SNAPSHOT_MODE_RANDOM_DELETE', 1);
-define('SNAPSHOT_MODE_RANDOM_DELETE_AND_RENAME', 2);
-define('SNAPSHOT_MODES', 3);
-
-function createBaseCalendar(): vcalendar
-{
-    echo 'Initializing base calendar' . PHP_EOL;
-    $base = new vcalendar([
-        'unique_id' => 'battis/ics-munger'
-    ]);
-    $base->setCalscale('GREGORIAN');
-    $base->setMethod('PUBLISH');
-
-    $tz = new vtimezone();
-    $tz->parse('BEGIN:VTIMEZONE
-TZID:America/New_York
-X-LIC-LOCATION:America/New_York
-BEGIN:STANDARD
-DTSTART:20191103T020000
-TZOFFSETFROM:-0400
-TZOFFSETTO:-0500
-RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=1SU;BYMONTH=11
-TZNAME:Eastern Standard Time
-END:STANDARD
-BEGIN:DAYLIGHT
-DTSTART:20190310T020000
-TZOFFSETFROM:-0500
-TZOFFSETTO:-0400
-RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=2SU;BYMONTH=3
-TZNAME:Eastern Daylight Time
-END:DAYLIGHT
-END:VTIMEZONE
-');
-    $base->addComponent($tz);
-
-    $base->setConfig([
-        'directory' => __DIR__ . '/calendars',
-        'filename' => 'base.ics'
-    ]);
-    return $base;
-}
-
-function createSnapshotSet(vcalendar $base): array
-{
-    $snapshot = [];
-    for ($i = 0; $i < SNAPSHOTS; $i++) {
-        $snapshot[$i] = clone $base;
-    }
-    return $snapshot;
-}
-
-function generateRandomEvents(vcalendar &$baseCalendar)
-{
-    echo 'Generating random events between 1/1/' . START_YEAR . ' and 12/31/' . END_YEAR . PHP_EOL;
-    $counter = 0;
-    $first = true;
-    $lastday = $lastmonth = $lastyear = 0;
-    for ($year = START_YEAR; $year <= END_YEAR; $year++) {
-        $lastyear = $year;
-        for ($month = 1; $month <= 12; $month++) {
-            $lastmonth = $month;
-            for ($day = rand(1, 7); $day <= ($month < 8 && $month % 2 === 1 || $month > 7 && $month % 2 === 0 ? 31 : ($month !== 2 ? 30 : ($year % 4 === 0 ? 29 : 28))); $day += rand(1, 7)) {
-                $lastday = $day;
-                if ($first) {
-                    $first = false;
-                    echo "  $month/$day/$year thru ";
-                }
-                $e = new vevent();
-                $e->setSummary(random_factor());
-                $e->setDtstart($year, $month, $day, rand(0, 23), rand(0, 59));
-                $e->setDuration(0, 0, rand(0, 3), rand(1, 45));
-                $baseCalendar->addComponent($e);
-                $counter++;
-            }
+$calendarDir = __DIR__ . DIRECTORY_SEPARATOR . 'calendars';
+$base = new CalendarGenerator();
+$base->save($calendarDir, 'base.ics');
+$snapshots = $base->snapshots(3);
+$i = 0;
+foreach ($snapshots as $snapshot) {
+    $snapshot->save($calendarDir, "snapshot_$i.ics");
+    $filtered = $snapshot->filter(function (vevent $e): bool {
+        return rand(1, 10) != 3;
+    });
+    $filtered->save($calendarDir, "snapshot_randomFilter_$i.ics");
+    $transformed = $snapshot->transform(function (vevent $e): vevent {
+        if (rand(1, 5) == 3) {
+            $e->setSummary(random_factor());
         }
-    }
-    echo "$lastmonth/$lastday/$lastyear ($counter events)" . PHP_EOL;
-
-    echo '  Saving base calendar file' . PHP_EOL;
-    $baseCalendar->saveCalendar();
+        return $e;
+    });
+    $transformed = $transformed->filter(function (vevent $e): bool {
+        return rand(1, 30) != 17;
+    });
+    $transformed->save($calendarDir, "snapshot_randomRename_$i.ics");
 }
-
-function saveSnapshots(vcalendar $baseCalendar, array $snapshotSet, int $mode = SNAPSHOT_MODE_SIMPLE): void
-{
-    echo 'Snapshotting overlapping windows from base calendar ';
-    switch ($mode) {
-        case SNAPSHOT_MODE_RANDOM_DELETE_AND_RENAME:
-            echo 'with random deletions and modifications';
-            break;
-        case SNAPSHOT_MODE_RANDOM_DELETE:
-            echo 'with random deletions';
-            break;
-        case SNAPSHOT_MODE_SIMPLE:
-        default:
-            // do nothing
-    }
-    echo PHP_EOL;
-    $window = intdiv(END_YEAR - START_YEAR, SNAPSHOTS - 1);
-    $step = intdiv(END_YEAR - START_YEAR, SNAPSHOTS + 1);
-    for ($i = 0; $i < SNAPSHOTS; $i++) {
-        $start = START_YEAR + ($i * $step);
-        $end = START_YEAR + ($i * $step) + $window + 1;
-        $counter = 0;
-        echo "  1/1/$start thru 12/31/$end ";
-        $baseCalendar->getComponent(0); // reset internal component counter
-        if (($selection = $baseCalendar->selectComponents($start, 1, 1, $end, 12, 31)) !== false) {
-            foreach ($selection as $year) {
-                foreach ($year as $month) {
-                    foreach ($month as $day) {
-                        foreach ($day as $e) {
-                            switch ($mode) {
-                                case SNAPSHOT_MODE_RANDOM_DELETE_AND_RENAME:
-                                    $include = rand(1, 10) !== 7;
-                                    if (rand(1, 10) === 4) {
-                                        $e->setSummary(random_factor());
-                                    }
-                                    break;
-                                case SNAPSHOT_MODE_RANDOM_DELETE:
-                                    $include = rand(1, 5) !== 3;
-                                    break;
-                                case SNAPSHOT_MODE_SIMPLE:
-                                default:
-                                    $include = true;
-                            }
-                            if ($include) {
-                                $snapshotSet[$i]->addComponent($e);
-                                $counter++;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        echo "($counter events selected)" . PHP_EOL;
-    }
-
-    echo '  Saving snapshot calendar files' . PHP_EOL;
-    for ($i = 0; $i < count($snapshotSet); $i++) {
-        switch ($mode) {
-            case SNAPSHOT_MODE_RANDOM_DELETE_AND_RENAME:
-                $filename = "snapshot_randomRename_$i.ics";
-                break;
-            case SNAPSHOT_MODE_RANDOM_DELETE:
-                $filename = "snapshot_randomFilter_$i.ics";
-                break;
-            case SNAPSHOT_MODE_SIMPLE:
-            default:
-                $filename = "snapshot_$i.ics";
-        }
-        echo "    Saving $filename" . PHP_EOL;
-        $snapshotSet[$i]->setConfig(['filename' => $filename]);
-        $snapshotSet[$i]->saveCalendar();
-    }
-}
-
-$base = createBaseCalendar();
-$snapshotSets = [];
-for ($i = 0; $i < SNAPSHOT_MODES; $i++) {
-    $snapshotSets[$i] = createSnapshotSet($base);
-}
-generateRandomEvents($base);
-for ($i = 0; $i < SNAPSHOT_MODES; $i++) {
-    saveSnapshots($base, $snapshotSets[$i], $i);
-}
-
-echo 'Done' . PHP_EOL;
+$base->filter(function (vevent $e): bool {
+    return preg_match('/([a-z])\\1/i', $e->getProperty('summary')) === 1;
+})->save($calendarDir, 'silly_aunt_sally.ics');
+$base->filter(function (vevent $e): bool {
+    return $e->getProperty('summary') > 'lemon';
+})->save($calendarDir, 'after_lemon.ics');
+$base->filter(function (vevent $e): bool {
+    return $e->getProperty('summary') < 'mongoose';
+})->save($calendarDir, 'before_mongoose.ics');
